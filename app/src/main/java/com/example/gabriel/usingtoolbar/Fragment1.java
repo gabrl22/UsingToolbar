777684861647162b283bb7da6c07605c8339e9c6
Gabriel Cole <lgcw92@gmail.com>
2015-08-26 00:45:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/gabriel/usingtoolbar/Fragment1.java
@@ -0,0 +1,36 @@
+package com.example.gabriel.usingtoolbar;
+
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class Fragment1 extends Fragment {
+
+    public static Fragment1 newInstance() {
+        Fragment1 fragment = new Fragment1();
+        return fragment;
+    }
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gabriel/usingtoolbar/Fragment2.java
@@ -0,0 +1,34 @@
+package com.example.gabriel.usingtoolbar;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment2 extends Fragment {
+
+    public static Fragment2 newInstance() {
+        Fragment2 fragment = new Fragment2();
+        return fragment;
+    }
+
+    public Fragment2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment2, container, false);
+    }
+
+
+}
